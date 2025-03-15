15584e65e4bdab8019ebd496c1f4f23da0c93c5d
Tanh1625 <dotuananh1625@gmail.com>
2025-03-15 15:03:35 +0700

    a

--- src/main/java/com/group3/sp25hsf302group3se1889vj/repository/BrandRepository.java
+++ src/main/java/com/group3/sp25hsf302group3se1889vj/repository/BrandRepository.java
@@ -2,10 +2,12 @@ package com.group3.sp25hsf302group3se1889vj.repository;
 
 import com.group3.sp25hsf302group3se1889vj.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface BrandRepository extends JpaRepository<Brand, Long> {
+public interface BrandRepository extends JpaRepository<Brand, Long>, JpaSpecificationExecutor<Brand> {
+
 }
--- src/main/java/com/group3/sp25hsf302group3se1889vj/service/impl/BrandServiceImpl.java
+++ src/main/java/com/group3/sp25hsf302group3se1889vj/service/impl/BrandServiceImpl.java
@@ -2,18 +2,30 @@ package com.group3.sp25hsf302group3se1889vj.service.impl;
 
 import com.group3.sp25hsf302group3se1889vj.dto.BrandDTO;
 import com.group3.sp25hsf302group3se1889vj.dto.filter.BrandFilterDTO;
+import com.group3.sp25hsf302group3se1889vj.entity.Brand;
+import com.group3.sp25hsf302group3se1889vj.entity.Category;
+import com.group3.sp25hsf302group3se1889vj.mapper.BrandMapper;
+import com.group3.sp25hsf302group3se1889vj.repository.BrandRepository;
 import com.group3.sp25hsf302group3se1889vj.service.BrandService;
+import com.group3.sp25hsf302group3se1889vj.specification.BrandSpecification;
+import com.group3.sp25hsf302group3se1889vj.specification.CategorySpecification;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class BrandServiceImpl implements BrandService {
+    private final BrandRepository brandRepository;
+    private final BrandMapper brandMapper;
+
     @Override
     public Page<BrandDTO> findAll(BrandFilterDTO filterDTO, Pageable pageable) {
-        return null;
+        Specification<Brand> specification = BrandSpecification.filterBrand(filterDTO);
+        return brandRepository.findAll(specification, pageable)
+                .map(brandMapper::toDTO);
     }
 
     @Override
